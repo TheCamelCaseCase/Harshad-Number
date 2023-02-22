4590d142fb1deb40c93f4b8378a9400065f61b13
CriminalCamelCase <125468315+CriminalCamelCase@users.noreply.github.com>
2023-02-22 14:49:05 +0100

    Update Calculation.java

--- HarshadNumber/Calculation.java
+++ HarshadNumber/Calculation.java
@@ -1,6 +1,37 @@
-package HarshadNumber;
-
 public class Calculation {
+    public static void getHarshadNumbers(int limit) {
+        getHarshadNumbers(1, limit);
+    }
+
+    public static void getHarshadNumbers(int start, int limit) {
+        if (start <= 0)
+            start = 1;
+        for (int number = start; number <= limit; number++) {
+            int n = 0;
+            int numberCopy = number;
+            while (numberCopy != 0) {
+                numberCopy /= 10;
+                n++;
+            }
+            int[] array = new int[n];
+            int subtract = 0;
+            int digitSum = 0;
+            for (int ii = 0; ii < array.length; ii++) {
+                n--;
+                double pow = Math.pow(10, n);
+                array[ii] = number / (int) pow;
+                if (ii == 0)
+                    subtract = array[0] * (int) pow;
+                if (ii > 0) {
+                    array[ii] = (number - subtract) / (int) pow;
+                    subtract += array[ii] * (int) pow;
+                }
+                digitSum += array[ii];
+            }
+            if (number % digitSum == 0)
+                System.out.println(number);
+        }
+    }
     public static void isHarshadNumber(int number) {
         int n = 0;
         int numberCopy = number;
@@ -23,9 +54,6 @@ public class Calculation {
             }
             digitSum += array[i];
         }
-        if (number % digitSum == 0)
-            System.out.println("You have found a harshad number.");
-        else
-            System.out.println("The number you have typed in is not a harshad number");
+        System.out.println(number % digitSum == 0);
     }
 }
